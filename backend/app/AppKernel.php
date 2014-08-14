c0e6c92a3606cdeff6754f1f6335178e88e6983f
puterakahfi <puterakahfi@gmail.com>
2014-08-14 16:44:04 +0700

    update composer, kernel

--- backend/app/AppKernel.php
+++ backend/app/AppKernel.php
@@ -16,6 +16,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Cms\AuthBundle\CmsAuthBundle(),
+            new Cms\ContentBundle\CmsContentBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
