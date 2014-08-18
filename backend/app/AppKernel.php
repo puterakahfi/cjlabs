d0d35325cf3e48bab89ab3ee78146f3372673f84
puterakahfi <puterakahfi@gmail.com>
2014-08-18 07:45:05 +0700

    updated composer, login, routing

--- backend/app/AppKernel.php
+++ backend/app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Cms\AuthBundle\CmsAuthBundle(),
             new Cms\ContentBundle\CmsContentBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
