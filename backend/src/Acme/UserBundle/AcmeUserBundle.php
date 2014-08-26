eec6fb287fa2bf706c2a4698a54527082d26b16c
puterakahfi <puterakahfi@gmail.com>
2014-08-26 08:11:52 +0700

    override fosbundle

---
+++ backend/src/Acme/UserBundle/AcmeUserBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Acme\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AcmeUserBundle extends Bundle
+{
+
+
+    public function getParent()
+    {
+        return 'FOSUserBundle';
+    }
+}
