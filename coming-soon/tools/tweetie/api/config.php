c8134f1bb96a0171e6452ef0835f1405410c7d7d
puterakahfi <puterakahfi@gmail.com>
2014-11-13 18:31:56 +0700

    added coming soon landing page

---
+++ coming-soon/tools/tweetie/api/config.php
@@ -0,0 +1,12 @@
+<?php
+    /**
+     * Your Twitter App Info
+     */
+    
+    // Consumer Key
+    define('CONSUMER_KEY', 'lKtT9Qu9jy2RrrZZCyvSWg');
+    define('CONSUMER_SECRET', 'piW79RGYiOXygvRQTLXJy2QALaxdkAFZzXBYeQTDqyc');
+
+    // User Access Token
+    define('ACCESS_TOKEN', '88625100-tqbk5WKRyuHLFe1A6SG0Cwr4RXzkzMXVcSQeSysI');
+    define('ACCESS_SECRET', 'ZpgpmMVuikDFRFvPDyvjKZ4lyXhqhNpEJNnfabYWw');
