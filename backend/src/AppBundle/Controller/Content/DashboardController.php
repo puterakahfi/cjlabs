13bc9a576d7dbd5f800c6da6dbb3651a30974277
puterakahfi <puterakahfi@gmail.com>
2014-11-21 19:07:06 +0700

    added entity security

--- backend/src/AppBundle/Controller/Content/DashboardController.php
+++ backend/src/AppBundle/Controller/Content/DashboardController.php
@@ -6,9 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-/**
- * @Route("/dahsboard")
- */
+
 class DashboardController extends Controller
 {
 
