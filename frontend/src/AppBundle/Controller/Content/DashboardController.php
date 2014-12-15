f12b94d2dd139ee15875db6ef4b5879e59d7746a
puterakahfi <puterakahfi@gmail.com>
2014-12-15 17:43:06 +0700

    update symfony version to 2.6

--- frontend/src/AppBundle/Controller/Content/DashboardController.php
+++ frontend/src/AppBundle/Controller/Content/DashboardController.php
@@ -7,7 +7,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 /**
- * @Route("/dashboard")
+ * @Route("/")
  */
 class DashboardController extends Controller
 {
--- frontend/src/LearnBundle/Controller/FormController.php
+++ frontend/src/LearnBundle/Controller/FormController.php
@@ -8,7 +8,9 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use LearnBundle\Entity\Task;
 use Symfony\Component\HttpFoundation\Request;
 
-
+/**
+ * @Route("/learn/form")
+ */
 class FormController extends Controller
 {
 
@@ -30,6 +32,35 @@ class FormController extends Controller
                 ->getForm();
 
         return $this->render('LearnBundle:Form:index.html.twig', array(
+                    'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * 
+     * @Route("/simple",  name="learn_form_simple")
+     */
+    public function simpleAction(Request $request)
+    {
+
+        $task = new Task();
+        $task->setTask('Write a blog post');
+        $task->setDueDate(new \DateTime('tomorrow'));
+        $form = $this->createFormBuilder($task)
+                ->add('task', 'text')
+                ->add('dueDate', 'date')
+                ->add('desc', 'textarea')
+                ->add('save', 'submit', array('label' => 'Create Task'))
+                ->getForm();
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            // perform some action, such as saving the task to the database
+            return $this->redirect($this->generateUrl('dashboard'));
+        }
+
+
+        return $this->render('LearnBundle:Form:simple.html.twig', array(
                     'form' => $form->createView(),
         ));
     }
