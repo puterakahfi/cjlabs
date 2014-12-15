6ebca42d06f00c18acaaa7fe55dcfd43e7bafff7
puterakahfi <puterakahfi@gmail.com>
2014-12-15 16:44:13 +0700

    added assets

--- backend/app/AppKernel.php
+++ backend/app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
--- frontend/src/AppBundle/Controller/Content/DashboardController.php
+++ frontend/src/AppBundle/Controller/Content/DashboardController.php
@@ -6,7 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-
+/**
+ * @Route("/dashboard")
+ */
 class DashboardController extends Controller
 {
 
